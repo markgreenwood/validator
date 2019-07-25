5b7fc287c47e410bcf1c9a2135ce2b2c9972ce1c
Mark Greenwood <mrkjgreenwood@gmail.com>
2019-07-25 07:35:03 -0700

    Ch. 2 Comments on how this is implemented

--- src/factory.ts
+++ src/factory.ts
@@ -4,9 +4,13 @@ import nonDivisibleValidationRule from "./rules/nonDivisible";
 
 interface ruleDescription {
   type: string;
-  options: any;
+  options?: {
+    divisor?: number,
+    error?: string
+  };
 };
 
+// list of rule types and the function that creates them
 const ruleFactoryMap: any = {
   nonPositive: function(): rule {
     return nonPositiveValidationRule;
@@ -16,10 +20,13 @@ const ruleFactoryMap: any = {
   },
 };
 
+// takes a rule description and converts it to a rule
 function toValidatorRule(ruleDescription: ruleDescription): rule {
   return ruleFactoryMap[ruleDescription.type](ruleDescription.options);
 }
 
+// findConfiguration takes a ruleset name and returns an array of rule descriptions;
+// the resulting array of rule descriptions is mapped to an array of rules
 export default function(findConfiguration: (ruleset: string) => ruleDescription[]) {
   return function(ruleSetName: string) {
     return validatorWith(findConfiguration(ruleSetName).map(toValidatorRule));
